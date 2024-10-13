74d60536d275bedade6c6108e79e0f2cba9c9d86
Ruslan <ruslan.kuga@keyconcept.at>
2024-10-13 13:24:15 +0300

    fix imports

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	chat "chat-server/pkg/chat_v1"
 	"fmt"
-	"github.com/fatih/color"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	chat.UnimplementedChatV1Server
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	chat.RegisterChatV1Server(s, &server{})
+
+	log.Printf("Listening")
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("falied to serve")
+	}
 }
